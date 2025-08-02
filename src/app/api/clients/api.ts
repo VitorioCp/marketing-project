25cd266cfdfe744e7efaa5138c43f5911bd8e3e9
Vitório Corrêa <97925377+VitorioCp@users.noreply.github.com>
2025-08-02 13:03:25 -0300

    fix: api

--- src/app/api/clients/api.ts
+++ src/app/api/clients/api.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const Api = axios.create({
-  baseURL: 'https://agentezap-backend.gtrphk.easypanel.host',
-})
+  baseURL: "https://marketin-site-backend.gtrphk.easypanel.host",
+});
